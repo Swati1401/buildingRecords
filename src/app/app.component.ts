5f36fccfdbf83e0a504e273052fc45fbfd669ad8
adarsh2070 <adarshverma2070@gmail.com>
2021-11-25 21:18:09 +0530

    added the raw data to check Working of Filter ,Sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,25 +18,113 @@ export class AppComponent {
       Property: {
         title: 'Property Type'
       },
-      email: {
-        title: 'CongigurationName'
+      Price: {
+        title: 'Price'
       }
     }
   };
   
   data = [
     {
-      Name: 1,
-      Tower: "Leanne Graham",
-      Property: "Bret",
-      email: "Sincere@april.biz"
+      Name: "Enclave",
+      Tower: "First",
+      Property: "One-BedRoom",
+      Price: 20000
     },
     // ... other rows here
     {
-      Name: 11,
-      Tower: "Nicholas DuBuque",
-      Property: "Nicholas.Stanton",
-      email: "Rey.Padberg@rosamond.biz"
+      Name: "Alpha",
+      Tower: "Second",
+      Property: "Two-BedRoom",
+      Price: 40000
+    },
+    {
+      Name: "Beta",
+      Tower: "Third",
+      Property: "three-BedRoom",
+      Price: 60000
+    },
+    {
+      Name: "Delta",
+      Tower: "Fourth",
+      Property: "Four-BedRoom",
+      Price: 80000
+    },
+    {
+      Name: "Cimage",
+      Tower: "Fifth",
+      Property: "Two-BedRoom",
+      Price: 60000
+    },
+    {
+      Name: "Green Tea",
+      Tower: "First",
+      Property: "Four-BedRoom",
+      Price: 100000
+    },
+    {
+      Name: "Surya",
+      Tower: "First",
+      Property: "One-BedRoom",
+      Price: 100000
+    },
+    {
+      Name: "Texa",
+      Tower: "First",
+      Property: "Four-BedRoom",
+      Price: 900000
+    },
+    {
+      Name: "Delta-D",
+      Tower: "Third",
+      Property: "Single-BedRoom",
+      Price: 900000
+    },
+    {
+      Name: "vista ",
+      Tower: "Five",
+      Property: "Two-BedRoom",
+      Price: 62000
+    },
+    {
+      Name: "Aroma ",
+      Tower: "First",
+      Property: "Two-BedRoom",
+      Price: 85000
+    },
+    {
+      Name: "Nexa ",
+      Tower: "Five",
+      Property: "Two-BedRoom",
+      Price: 62000
+    },
+    {
+      Name: "Rexa ",
+      Tower: "First",
+      Property: "Two-BedRoom",
+      Price: 32000
+    },
+    {
+      Name: "Decon ",
+      Tower: "Three",
+      Property: "One-BedRoom",
+      Price: 20000
+    },
+    {
+      Name: "TDI ",
+      Tower: "Five",
+      Property: "Three-BedRoom",
+      Price: 62000
+    },
+    {
+      Name: "TDI ",
+      Tower: "Second",
+      Property: "One-BedRoom",
+      Price: 62000
     }
   ];
+
+
+
+  
 }
