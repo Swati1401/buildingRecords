2270a5b8b160379b22d5bea8046e7dfd36d9ea1b
Swati Kushwaha <swati1401@gmail.com>
2021-11-25 18:41:59 +0530

    created multilevel Dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BuildingRecords';
+  settings = {
+    columns: {
+      Name: {
+        title: 'Building Name'
+      },
+      Tower: {
+        title: 'Tower'
+      },
+      Property: {
+        title: 'Property Type'
+      },
+      email: {
+        title: 'CongigurationName'
+      }
+    }
+  };
+  
+  data = [
+    {
+      Name: 1,
+      Tower: "Leanne Graham",
+      Property: "Bret",
+      email: "Sincere@april.biz"
+    },
+    // ... other rows here
+    {
+      Name: 11,
+      Tower: "Nicholas DuBuque",
+      Property: "Nicholas.Stanton",
+      email: "Rey.Padberg@rosamond.biz"
+    }
+  ];
 }
